d5291bf99798c279d44240a038e6dc2cb3775857
Yunus Kocaman <156374474+ykunus@users.noreply.github.com>
2024-05-17 17:03:42 -0400

    Update TwoSums.java

--- TwoSums.java
+++ TwoSums.java
@@ -1,1 +1,21 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap <Integer, Integer> numbers = new HashMap<>();
+        int[] solution= new int[2]; 
+        int indexPos = 0;
+        for( int i = 0 ; i < nums.length; i++) {
+        	numbers.put(nums[i], i );
+        }
+        for(int i = 0 ; i < nums.length; i++){
+            int difference = target - nums[i];
+            if(numbers.containsKey(difference) && numbers.get(difference)!= i){
+            	
+                return new int []{i , numbers.get(difference)};
+            }
+        }
+        return solution;
+    }
+}
 
