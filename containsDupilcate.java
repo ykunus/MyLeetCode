117da03232ebe0dd861752aa44b352f4d16cc7fb
Yunus Kocaman <156374474+ykunus@users.noreply.github.com>
2024-05-17 17:04:33 -0400

    Update containsDupilcate.java

--- containsDupilcate.java
+++ containsDupilcate.java
@@ -1,1 +1,15 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+		HashSet<Integer> noDupNums = new HashSet<>();
+		for(int i = 0; i< nums.length; i ++) {
+			noDupNums.add(nums[i]);
+		}	
+		if(noDupNums.size() == nums.length){
+			return false;
+		}else {
+			return true;
+		}
+	}
+}
